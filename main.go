51f89289fb2b7b4c71641b8b719eee3469350e1d
ArtDark <balusoff@gmail.com>
2020-06-09 09:45:32 +0300

    Deleted if/else construction. Added change types in formula

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 func main() {
-	var balance int32 = 1_500_000_000  // 15 миллионов в копейках
-	var transfer int32 = 1_000_000_000 // 10 миллионов в копейках
-	total := balance + transfer        // int32 + int32 будет int32
+	var balance int32 = 1_500_000_000         // 15 миллионов в копейках
+	var transfer int32 = 1_000_000_000        // 10 миллионов в копейках
+	total := int64(balance) + int64(transfer) // int32 + int32 будет int32
 
-	if total < balance && total < transfer {
-		println(int64(balance) + int64(transfer))
-	} else {
-		println(total)
-	}
+	println(total)
 
 }
